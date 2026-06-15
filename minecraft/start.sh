75abb9ca5239bc8f330032d3e4616162eaa820b1
Kieran Glover <kieran.glovjunk@gmail.com>
2026-06-15 19:30:34 -0400

    Construct server properties using variables on install.sh

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -3,9 +3,9 @@
 source /opt/minecraft/.config
 
 if [ "$EDITION" = "java" ]; then
-	java -Xmx2G -Xms2G -jar server.jar nogui
+	java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY -jar server.jar nogui
 elif [ "$EDITION" = "bedrock" ]; then
-	./bedrock_server
+	exec screen -DmS minecraft ./bedrock_server
 else
 	echo "Unknown edition: $EDITION"
 	exit 1
