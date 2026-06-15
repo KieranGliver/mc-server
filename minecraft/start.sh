21d6a80a1ae989a9bcde02ff5bf08c102b54a8c8
Kieran Glover <kieran.glovjunk@gmail.com>
2026-06-15 19:56:04 -0400

    Wipe screens on start of bedrock servers

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -5,6 +5,7 @@ source /opt/minecraft/.config
 if [ "$EDITION" = "java" ]; then
 	java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY -jar server.jar nogui
 elif [ "$EDITION" = "bedrock" ]; then
+	screen -wipe
 	exec screen -DmS minecraft ./bedrock_server
 else
 	echo "Unknown edition: $EDITION"
